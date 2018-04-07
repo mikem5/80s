09f53fb81f062fc0965f94e2121cf95c1a2ae3ed
mikem5 <mikem5@users.noreply.github.com>
2018-04-07 12:20:38 -0600

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ import tld
 
 
 
-conn = psycopg2.connect("host=192.168.3.15 dbname=eight user=not_public password=not_public")
+conn = psycopg2.connect("host=not_public dbname=eight user=not_public password=not_public")
 cur = conn.cursor()
 
 
