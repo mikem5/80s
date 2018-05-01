91c149fd284d3c7450e3978791c11e8d83f19947
mikem <mikem@bijection.net>
2018-05-01 10:10:18 -0600

    change structure to actually pull from database on each request

--- app/routes.py
+++ app/routes.py
@@ -5,89 +5,130 @@ import datetime
 import tld
 
 
-with open('db.info', 'r') as dbfile:
+
+# read db info from local file
+with open('/var/www/80s/app/db.info', 'r') as dbfile:
     db_pw = dbfile.read().rstrip()
 
 
-conn = psycopg2.connect(db_pw)
 
-# need this or remote server breaks
-conn.set_client_encoding("utf-8")
+# pull from database depending on which table
+def db_pull(table):
 
-cur = conn.cursor()
+
+    # establish db connection
+    conn = psycopg2.connect(db_pw)
+
+    # need this or remote server breaks
+    conn.set_client_encoding("utf-8")
+    cur = conn.cursor()
+
+    if table == 'bandcamp':
+        dt = datetime.datetime.now() - datetime.timedelta(7)
+        cur.execute("SELECT * FROM bandcamp WHERE published > (%s)", (dt,))
+    
+    if table == 'youtube':
+        cur.execute("SELECT * FROM youtube;  ")
+
+    if table == 'images':
+        dt = datetime.datetime.now() - datetime.timedelta(2)
+        cur.execute("SELECT * FROM images WHERE date > (%s)", (dt,))
+
+
+    rows = cur.fetchall()
+    conn.commit()
+    conn.close()
+
+    return rows
 
 
 
 
 # bandcamp generation
-dt = datetime.datetime.now() - datetime.timedelta(7)
-cur.execute("SELECT * FROM bandcamp WHERE published > (%s)", (dt,))
-bc_rows = cur.fetchall()
-conn.commit()
+def bandcamp_sort():
 
-bc_rows = sorted(bc_rows, key=lambda x: x[6], reverse=True)
+    bc_rows = db_pull('bandcamp')
 
+    bc_rows = sorted(bc_rows, key=lambda x: x[6], reverse=True)
 
-bc_cost = []
-bc_zero = []
+    bc_cost = []
+    bc_zero = []
 
-for x in bc_rows:
-    if x[6] < datetime.datetime.now():
+    for x in bc_rows:
+        if x[6] < datetime.datetime.now():
 
-        di = {'meta': " - ".join([x[3],x[4]]),
-            'img' :x[7].split('/',1)[1],
-            'price': x[5],
-            'date' : x[6],
-            'url' : x[2]
-            }
-        if x[5] == '0':
-            bc_zero.append(di)
+            di = {'meta': " - ".join([x[3],x[4]]),
+                'img' :x[7].split('/',1)[1],
+                'price': x[5],
+                'date' : x[6],
+                'url' : x[2]
+                }
+            if x[5] == '0':
+                bc_zero.append(di)
+            else:
+                bc_cost.append(di)
         else:
-            bc_cost.append(di)
-    else:
-        pass
+            pass
+
+    # returns a tuple
+    return (bc_zero, bc_cost)
+
+
+
 
 
 # youtube generation
-cur.execute("SELECT * FROM youtube;  ")
-yt_rows = cur.fetchall()
-conn.commit()
+def youtube_sort():
+    
+    yt_rows = db_pull('youtube')
 
-yt_rows = sorted(yt_rows, key=lambda x: x[1], reverse=True)
+    yt_rows = sorted(yt_rows, key=lambda x: x[1], reverse=True)
 
-yt_posts = []
+    yt_posts = []
 
-for i in range(8):
-    x = yt_rows[i]
-    if tld.get_tld(x[2]) == 'youtu.be':
+    for i in range(8):
+        x = yt_rows[i]
+        if tld.get_tld(x[2]) == 'youtu.be':
 
-        di = {'id': x[2].split('/')[-1]
-            }
-    else:
-        di = {'id': x[2].split('=')[-1]
-            }
-    yt_posts.append(di)
+            di = {'id': x[2].split('/')[-1]
+                }
+        else:
+            di = {'id': x[2].split('=')[-1]
+                }
+        yt_posts.append(di)
+
+    return yt_posts
+
 
 
 
 # images generation
-dt = datetime.datetime.now() - datetime.timedelta(2)
-cur.execute("SELECT * FROM images WHERE date > (%s)", (dt,))
-img_rows = cur.fetchall()
-conn.commit()
+def image_sort():
 
-img_rows = sorted(img_rows, key=lambda x: x[1], reverse=True)
+    img_rows = db_pull('images')
 
-img_posts =[]
-for x in img_rows:
-    di = {'img' : x[3].split('/',1)[-1] }
-    img_posts.append(di)
+    img_rows = sorted(img_rows, key=lambda x: x[1], reverse=True)
+
+    img_posts =[]
+    for x in img_rows:
+        di = {'img' : x[3].split('/',1)[-1] }
+        img_posts.append(di)
+
+
+    return img_posts
+
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    return render_template('index.html', zero=bc_zero, cost=bc_cost, yt=yt_posts, imgs = img_posts)
+    (bc_z, bc_c) = bandcamp_sort()
+    yt_p = youtube_sort()
+    img_p = image_sort()
+
+
+    return render_template('index.html', zero=bc_z, cost=bc_c, yt=yt_p, imgs = img_p)
 
 
